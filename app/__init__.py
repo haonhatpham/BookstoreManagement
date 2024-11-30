5505de392a95d2077ca003e8b0a299dd5383de70
HieuLT24 <duchieuphung5@gmail.com>
2024-11-30 14:17:53 +0700

    update from main repo

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.secret_key = 'HJGGHD*^&R$YGFGHDYTRER&*TRTYCHG^R&^T'
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:%s@localhost/bookstore3hdb?charset=utf8mb4" % quote("Admin@123")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-# app.config["PAGE_SIZE"] = 8
+app.config["PAGE_SIZE"] = 12
 db = SQLAlchemy(app)
 
 cloudinary.config(cloud_name='dtcxjo4ns',
--- app/dao.py
+++ app/dao.py
@@ -1,14 +1,35 @@
-# from app.models import Category, Product, User
+from app.models import Category, Book, User, Review
 from app import app, db
+from flask import request, jsonify
 import hashlib
 import cloudinary.uploader
+from sqlalchemy import desc
+
+def load_banner():
+    books_banner = Book.query.limit(4).all()
+    return books_banner
+
+def load_feature_book():
+    feature_book =  Book.query.offset(4).limit(6).all()
+    return feature_book
 
 
 def load_categories():
-    pass
+    return Category.query.order_by('id').limit(8).all()
 
-def load_products(kw=None, cate_id=None, page=1):
-    pass
+def load_new_products(kw=None, cate_id=None, page=1):
+    query = Book.query.order_by(desc(Book.id))
+    if kw:
+        query = query.filter(Book.name.contains(kw))
+    if cate_id:
+        query = query.filter(Book.category_id == cate_id)
+    page_size = app.config["PAGE_SIZE"]
+    start = (page - 1) * page_size
+    query = query.slice(start, start + page_size)
+    return query.all()
+
+def count_products():
+    return Book.query.count()
 
 def add_user(name, username, password, avatar):
     pass
@@ -18,3 +39,21 @@ def auth_user(username, password):
 
 def get_user_by_id(id):
     pass
+
+def rate_product():
+    data = request.json
+    product_id = data.get("book_id")
+    rating_value = data.get("rating")
+    if not (1<= rating_value <=5):
+        return jsonify({'error': 'Invalid rating value'}), 400
+    new_rating = Review(book_id=product_id, rating=rating_value)
+    db.session.add(new_rating)
+    db.session.commit()
+    return jsonify({'message': 'Rating submitted successfully!'})
+
+def average_rating(book_id):
+    ratings = Review.query.filter_by(product_id=book_id).all()
+    if not ratings:
+        return jsonify({'average_rating': 0})
+    avg_rating = sum(r.rating for r in ratings) / len(ratings)
+    return jsonify({'average_rating': avg_rating})
