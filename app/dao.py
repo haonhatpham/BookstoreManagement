08c091d1ad5ce9ba19b30cf144c4badcdc26594c
hao pham <haopham081098@gmail.com>
2024-12-01 17:47:03 +0700

    dang nhap dang ky

--- app/dao.py
+++ app/dao.py
@@ -1,4 +1,4 @@
-from app.models import Category, Book, User
+from app.models import Category, Book, User, Role
 from app import app, db
 import hashlib
 import cloudinary.uploader
@@ -11,7 +11,7 @@ def load_banner():
 
 
 def load_feature_book():
-    feature_book =  Book.query.offset(4).limit(6).all()
+    feature_book = Book.query.offset(4).limit(6).all()
 
     return feature_book
 
@@ -35,16 +35,49 @@ def load_new_products(kw=None, cate_id=None, page=1):
 
     return query.all()
 
+
 def count_products():
     return Book.query.count()
 
-def add_user(name, username, password, avatar):
-    pass
+
+def add_user(name, username, password, email, phone, birth, gender, avatar):
+    password = str(hashlib.md5(password.encode('utf-8')).hexdigest())
+    user_role = Role.query.filter_by(name="User").first()
+    name_parts = name.strip().split(" ", 1)
+    if len(name_parts) > 1:
+        first_name = name_parts[1]
+        last_name = name_parts[0]
+    else:
+        # Nếu không có dấu cách, mặc định đặt last_name là name và first_name rỗng
+        first_name = ''
+        last_name = name_parts[0]
+    u = User(
+        first_name=first_name,
+        last_name=last_name,
+        username=username.strip(),
+        password=password,
+        email=email.strip(),
+        phone=phone.strip(),
+        birth=birth,
+        gender=gender,  # Nam active=True,
+        role_id=user_role.id
+    )
+
+    if avatar:
+        res = cloudinary.uploader.upload(avatar)
+        print(res)
+        u.avatar_file = res.get("secure_url")
+
+    db.session.add(u)
+    db.session.commit()
 
 
 def auth_user(username, password):
-    pass
+    password = hashlib.md5(password.encode('utf-8')).hexdigest()
+
+    return User.query.filter(User.username.__eq__(username.strip()),
+                             User.password.__eq__(password)).first()
 
 
 def get_user_by_id(id):
-    pass
+    return User.query.get(id)
